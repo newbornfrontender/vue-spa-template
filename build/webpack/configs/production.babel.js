18878f2e7b3e8e3636533914390b4da9ff0e8bc6
Infant Frontender <newbornfrontender@gmail.com>
2018-07-19 17:56:21 +0300

    Добавил комментарии и динамические настройки для плагинов

--- build/webpack/configs/production.babel.js
+++ build/webpack/configs/production.babel.js
@@ -1,44 +1,73 @@
 'use strict';
 
+// Утилиты
 // -----------------------------------------------------------------------------
 
 import {
   getResolvePath as resolve,
   getContentHash as contentHash,
+  getOptionPath as optionPath,
 } from '../modules/utils';
 
+// Webpack плагины
 // -----------------------------------------------------------------------------
 
 import WebpackMerge from 'webpack-merge';
 
+// Базовая конфигурация webpack
 // -----------------------------------------------------------------------------
 
-import baseConfig from './webpack.conf.common';
+import baseConfig from './common';
 
+// Webpack части: ./*
 // -----------------------------------------------------------------------------
 
-import output from './parts/output';
-import optimization from './parts/optimization';
+import output from '../parts/output';
+import optimization from '../parts/optimization';
 
+// Webpack части: ./plugins/*
 // -----------------------------------------------------------------------------
 
-import dynamicCdnWebpackPlugin from './parts/plugins/dynamic-cdn-webpack-plugin';
-import miniCssExtractPlugin from './parts/plugins/mini-css-extract-plugin';
+import dynamicCdnWebpackPlugin from '../parts/plugins/dynamic-cdn-webpack-plugin';
+import miniCssExtractPlugin from '../parts/plugins/mini-css-extract-plugin';
+
+const CDN_PLUGIN = () => {
+  const option = optionPath('../../options/webpack/optimization');
+
+  const CDN = option.cdn.enabled;
+  const PLUGIN = option.cdn.plugin;
+
+  if (CDN) {
+    switch (PLUGIN) {
+      case 'dynamic-cdn-webpack-plugin': return dynamicCdnWebpackPlugin;
+    };
+  };
+}
 
 const plugins = new WebpackMerge([
-  optimization,
-  key.cdn ?
-    dynamicCdnWebpackPlugin :
-    miniCssExtractPlugin(),
+  output,
+  CDN_PLUGIN,
+  miniCssExtractPlugin(),
 ]);
 
+// Production конфигурация webpack
 // -----------------------------------------------------------------------------
+
+const OPTIMIZATION = () => {
+  const option = optionPath('../../options/webpack/optimization');
+
+  const SPLIT_CHUNKS = option.splitChunks.enabled;
+
+  if (SPLIT_CHUNKS) return optimization;
+}
 
 const prodConfig = new WebpackMerge([
   output(),
-  plugins
+  OPTIMIZATION,
+  plugins,
 ]);
 
+// Конфигурация
 // -----------------------------------------------------------------------------
 
 export default new WebpackMerge([ baseConfig, prodConfig ]);
