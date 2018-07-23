ab956047f80fef716b97d488cdb586ab754071c4
Infant Frontender <newbornfrontender@gmail.com>
2018-07-23 13:27:34 +0300

    Вынес фунцию readOptions в utils.js

--- build/webpack/modules/keyreader.js
+++ build/webpack/modules/keyreader.js
@@ -1,19 +1,9 @@
 'use strict';
 
-const readOptions = (optionsPath) => {
-  let options;
-  try {
-    options = require(optionsPath);
-  } catch(err) {
-    console.error(`No config file found at: ${optionsPath}`);
-    options = {};
-  };
-  return options;
-};
+import { options } from './utils';
 
-const getWebpackConf = (filename) => {
-  let options = readOptions(`../../../options/webpack/${filename}`);
-  return options;
-};
+export default () => {
+  let config = options('../../../options.config');
 
-export { getWebpackConf };
+  return config;
+};
