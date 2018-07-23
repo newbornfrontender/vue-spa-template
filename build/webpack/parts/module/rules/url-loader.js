e9a9daefacf7505b2261be7d8794aae0d41166f8
Infant Frontender <newbornfrontender@gmail.com>
2018-07-23 13:33:55 +0300

    Сделал конфиг аналогично file-loader

--- build/webpack/parts/module/rules/url-loader.js
+++ build/webpack/parts/module/rules/url-loader.js
@@ -1,60 +1,68 @@
 'use strict';
 
-const img = ({ test, name, limit } = {}) => ({
-  module: {
-    rules: [
-      {
-        test,
-        // include,
-        // exclude,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit, // 10000
-            name,
+import { assets, hash } from '../../../modules/utils';
+
+export default {
+  img: ({ test, name, limit } = {}) => ({
+    module: {
+      rules: [
+        {
+          test: test || /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          // include,
+          // exclude,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: limit || 10000,
+              name: name || assets(`img/[name]${hash()}.[ext]${hash({
+                prefix: '?',
+              })}`),
+            },
           },
         },
-      },
-    ],
-  },
-});
+      ],
+    },
+  }),
 
-const media = ({ test, name, limit } = {}) => ({
-  module: {
-    rules: [
-      {
-        test,
-        // include,
-        // exclude,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit, // 10000
-            name,
+  cmedia: ({ test, name, limit } = {}) => ({
+    module: {
+      rules: [
+        {
+          est: test || /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+          // include,
+          // exclude,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: limit || 10000,
+              name: name || assets(`media/[name]${hash()}.[ext]${hash({
+                prefix: '?',
+              })}`),
+            },
           },
         },
-      },
-    ],
-  },
-});
+      ],
+    },
+  }),
 
-const fonts = ({ test, name, limit } = {}) => ({
-  module: {
-    rules: [
-      {
-        test,
-        // include,
-        // exclude,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit, // 10000
-            name,
+  fonts: ({ test, name, limit } = {}) => ({
+    module: {
+      rules: [
+        {
+          test: test || /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+          // include,
+          // exclude,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: limit || 10000,
+              name: name || assets(`fonts/[name]${hash()}.[ext]${hash({
+                prefix: '?',
+              })}`),
+            },
           },
         },
-      },
-    ],
-  },
-});
-
-export { img, media, fonts };
+      ],
+    },
+  }),
+};
